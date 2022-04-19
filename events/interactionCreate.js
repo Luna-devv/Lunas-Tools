ac4ea1aa25e412c497e75fca393905096061f2b3
Luna <71079641+Luna-devv@users.noreply.github.com>
2022-04-19 22:02:04 +0200

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,5 +2,23 @@ module.exports = {
     name: 'interactionCreate',
     run: async (interaction) => {
 
+        interaction.reply({
+            epemeral: true,
+            content: `Lol? YOu think this really works xd`,
+            components: [
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            label: `Waya Bot`,
+                            style: 5,
+                            url: `https://waya.one`
+                        }
+                    ]
+                }
+            ]
+        });
+
     }
-}
+};
