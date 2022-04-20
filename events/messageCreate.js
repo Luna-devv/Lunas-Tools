9763b9fd51c90c06b8b2c13497ee504a6bf6771e
Crni 39 <bornacrncic98@gmail.com>
2022-04-20 22:48:19 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -37,8 +37,8 @@ module.exports = {
                 if (!message.content && !message.embeds) return;
 
                 if (message.embeds[0]) {
-                    message.embeds[0].color = `#7289da`; message.embeds[0]?.author?.name?.replace('Crni39', 'The Digital');
-                    message.embeds[0].description = '> ' + message.embeds[0]?.description?.split('- Crni39\n').join('\n> ').slice(0, -2);
+                    message.embeds[0].color = `#7289da`; message.embeds[0]?.author.name = 'The Digital';
+                    message.embeds[0].description = '> ' + message.embeds[0]?.description?.split('- Crni39\n').join('\n> ').slice(0, -9);
                 };
 
                 client.channels.cache.get(`956604381300654101`).send({
