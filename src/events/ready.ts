7a1f7a6ef10785d8b85298350827ebf3def86cdc
Lunish <luni.siaa@gmail.com>
2022-08-14 21:24:43 +0200

    swap guild banner

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,6 @@
 import Logger from '../modules/logger';
+import path from 'path';
+import fs from 'fs';
 
 export default {
     name: 'ready',
@@ -9,12 +11,20 @@ export default {
         ['828676951023550495', '810248284861366332'].forEach((guild) => client.guilds.cache.get(guild)?.commands.set([]).catch(() => null)); // test guilds
         await client?.application?.commands?.set(require('../json/interactionsData').default); // all guilds
 
+        const banners = fs.readdirSync(path.join(__dirname, '../../banners'));
+        setInterval(() => {
+            const banner = banners[Math.floor(Math.random() * (banners.length - 1))];
+
+            const guild = client.guilds.cache.get('828676951023550495');
+            guild.setBanner(path.join(__dirname, `../../banners/${banner}`), 'banner swap')
+        }, 1000 * 60 * 16);
+
         // copy Luna's user status
         const user: any = client.guilds.cache.get('810248284861366332')?.presences?.cache?.get('821472922140803112');
 
         client?.user?.setPresence({ status: user?.status });
         client.lastStatus = user?.status;
-        
+
         Logger.end(`App`, `Connected as ${client?.user?.tag}.`, `blue`);
 
         // start twitter client
